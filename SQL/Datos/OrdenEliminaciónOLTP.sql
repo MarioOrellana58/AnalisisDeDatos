fbf6636c328dee4db743a9937ef30a671eba949a
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2021-10-23 02:28:11 -0600

    ETL con CSV y mas

---
+++ SQL/Datos/OrdenEliminaciónOLTP.sql
@@ -0,0 +1,19 @@
+
+
+--TAMOS
+DELETE FROM Cotizacion;
+DELETE FROM CotizacionDetalle;
+DELETE FROM Orden;
+DELETE FROM Detalle_orden;
+DELETE FROM Clientes;
+DELETE FROM Ciudad;
+DELETE FROM Region;
+DELETE FROM Pais;
+DELETE FROM Aseguradoras;
+DELETE FROM Partes;
+DELETE FROM Categoria;
+DELETE FROM Linea;
+DELETE FROM Descuento;
+DELETE FROM PlantaReparacion;
+DELETE FROM StatusOrden;
+DELETE FROM Vehiculo;
