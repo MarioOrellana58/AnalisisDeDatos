0dc3cc50e21aacf82f36255dbafe0eccc0f3140b
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2021-10-24 23:56:04 -0600

    Proyecto terminado

--- SQL/3.3 Actualizar OrigenOrden y Totales.sql
+++ SQL/3.3 Actualizar OrigenOrden y Totales.sql
@@ -2,7 +2,7 @@ UPDATE staging.OrdenCotizacion
 SET OrigenOrden = CASE
 					WHEN IDCotizacion IS NOT NULL THEN 'Aseguradora'
 					WHEN ID_Cliente IS NOT NULL THEN 'Cliente registrado'
-					ELSE 'Cliente no registrado'
+					ELSE 'Invitados'
 					END
 WHERE OrigenOrden IS NULL
 
