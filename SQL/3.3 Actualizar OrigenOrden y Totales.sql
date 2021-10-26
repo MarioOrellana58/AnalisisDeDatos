00f7f06a96d01f5eb8246785d2fc67720ac500d7
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2021-10-25 22:34:58 -0600

    Cambio menor a ETL de hechos

--- SQL/3.3 Actualizar OrigenOrden y Totales.sql
+++ SQL/3.3 Actualizar OrigenOrden y Totales.sql
@@ -8,14 +8,16 @@ WHERE OrigenOrden IS NULL
 
 
 UPDATE staging.OrdenCotizacion
-SET TotalCotizacion =  OC.CantidadCotizacion * P.Precio 
+SET TotalCotizacion =  OC.CantidadCotizacion * P.Precio * IIF(C.PorcentajeDescuento IS NOT NULL, 1 - C.PorcentajeDescuento, 1)
 		FROM staging.OrdenCotizacion OC
 			INNER JOIN Dimension.Partes P ON (OC.ID_Parte_Cotizacion = P.ID_Parte)
+			LEFT JOIN Dimension.Descuento C ON (OC.ID_Descuento = C.ID_Descuento)
 WHERE IDCotizacion IS NOT NULL
 
 
 UPDATE staging.OrdenCotizacion
-SET Total_Orden =  OC.CantidadOrden * P.Precio 
+SET Total_Orden =  OC.CantidadOrden * P.Precio * IIF(C.PorcentajeDescuento IS NOT NULL, 1 - C.PorcentajeDescuento, 1)
 		FROM staging.OrdenCotizacion OC
 			INNER JOIN Dimension.Partes P ON (OC.ID_Parte_Orden = P.ID_Parte)
+			LEFT JOIN Dimension.Descuento C ON (OC.ID_Descuento = C.ID_Descuento)
 WHERE ID_Orden IS NOT NULL
